ac2a69b630ce2b768a4800c816c6d4e28d76f0e0
Penelope Hill <penelope@squareup.com>
2015-07-23 16:01:56 -0700

    wip Sharing with Nick

--- WordPicker.py
+++ WordPicker.py
@@ -12,7 +12,23 @@ class WordPicker(luigi.Task):
         chosen_word = words[index]
         f = self.output().open('w')
         f.write(chosen_word)
-        f.close()
+class RefFile(luigi.Task):
+    """Read a 'reference' file from disk"""
+    def output(self):
+        return luigi.LocalTarget("output/word-picker/aRefFile.txt")
+
+class FlipWordBackwards(luigi.Task):
+    def requires(self):
+        return RefFile()
+
+    def output(self):
+        return luigi.LocalTarget('output/word-picker/reversed_word.txt')
+
+    def run(self):
+        print self.input()
+        with self.input().open('r') as raw_word:
+            word = raw_word.read()
+            print word 
 
 if __name__ == "__main__":
     luigi.run()
