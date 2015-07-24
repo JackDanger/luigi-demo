a48c9dbbe7e9a823e803c8f3cdbef35fa1a72f4e
Liz Rush <lizmrush@squareup.com>
2015-07-23 17:50:53 -0700

    Fixed file writing

--- WordPicker.py
+++ WordPicker.py
@@ -2,33 +2,31 @@ import luigi
 import random
 
 class WordPicker(luigi.Task):
-
     def output(self):
-        return luigi.LocalTarget('output/word-picker/word.txt')
-    
+        return luigi.LocalTarget('./output/word-picker/word.txt')
+
     def run(self):
         words = ["purple", "blue", "tooth", "beachball"]
         index = random.randint(0,3)
         chosen_word = words[index]
-        f = self.output().open('w')
-        f.write(chosen_word)
-class RefFile(luigi.Task):
-    """Read a 'reference' file from disk"""
-    def output(self):
-        return luigi.LocalTarget("output/word-picker/aRefFile.txt")
+
+        with self.output().open('w') as f:
+            f.write(chosen_word)
 
 class FlipWordBackwards(luigi.Task):
     def requires(self):
-        return RefFile()
+        return WordPicker()
 
     def output(self):
-        return luigi.LocalTarget('output/word-picker/reversed_word.txt')
+        return luigi.LocalTarget('./output/word-picker/reversed_word.txt')
 
     def run(self):
         print self.input()
         with self.input().open('r') as raw_word:
             word = raw_word.read()
-            print word 
+
+        with self.output().open('w') as f:
+            f.write('I just reversed {0}'.format(word[::-1]))
 
 if __name__ == "__main__":
     luigi.run()
